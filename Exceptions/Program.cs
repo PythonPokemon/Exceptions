0d3bc18fc348d2a4641a68a4b12005a4e30a4696
jakob.derzapf <jakob.derzapf@WASNEZOW>
2024-10-28 08:50:08 +0100

    Projektdateien hinzufügen.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var result = Calc(10, 0);
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Etwas geht schief!" + e);
+            }
+            finally
+            {
+                Console.WriteLine("wird immer ausgegeben");
+            }
+            
+            Console.WriteLine("Hello, World!");
+        }
+
+        static int Calc(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
